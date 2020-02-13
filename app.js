4fea3cde2ce574c85f6cb23fb71adde946295a0b
pramodkharade <kharade.pramod91@gmail.com>
2020-02-13 14:11:53 +0530

    added listner to click button

--- app.js
+++ app.js
@@ -7,11 +7,26 @@ let budgetController = (function() {}
 // UI Controller
 
 let UIController = (function() {
+    DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector()
+            }
+        },
+    };
 
 })();
 
 // APP controller
 
-let appController = (function() {
-
-})();
+let appController = (function(budgetCtrl, uiCtrl) {
+    document.querySelector('.add__btn').addEventListener('click', function() {
+        console.log("Calling");
+    });
+})(budgetController, UIController);
