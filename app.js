682ef6da2eb9aee2797babcda508b94307673a26
pramodkharade <kharade.pramod91@gmail.com>
2020-02-13 16:08:43 +0530

    added data structure to store data

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ let budgetController = (function() {
         this.description = description;
         this.value = value;
     };
+    let data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 })();
 
 // UI Controller
