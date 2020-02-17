b8577458ecd8c444ae6c4496aae51aa5443ce2bb
pramodkharade <kharade.pramod91@gmail.com>
2020-02-17 12:43:11 +0530

    init reset budget

--- app.js
+++ app.js
@@ -136,7 +136,11 @@ let UIController = (function() {
             document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
             document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
             document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
-            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '--';
+            }
         },
         getDomStrings: function() {
             return DOMstrings;
@@ -191,6 +195,12 @@ let appController = (function(budgetCtrl, uiCtrl) {
         init: function() {
             console.log('Application has been started');
             setupEventListners();
+            uiCtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
         }
     };
 
